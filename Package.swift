fc17b9f6dcff51b7237919ca9802bca06381c2de
Anthony Broad-Crawford <anthony@anthonybroadcrawford.com>
2020-09-21 21:14:49 -0500

    working draft on swift package manager file

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.1
+ // The swift-tools-version declares the minimum version of Swift required to build this package.
+
+ import PackageDescription
+
+ let package = Package(
+     name: "FSCalendar",
+     platforms: [
+         .iOS(.v8),
+     ],
+     products: [
+         .library(
+             name: "FSCalendar",
+             targets: ["FSCalendar"])
+     ],
+     targets: [
+         .target(
+             name: "FSCalendar",
+             path: "FSCalendar",
+             publicHeadersPath: "FSCalendar")
+     ]
+ )
